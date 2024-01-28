919c4f1f8eabd1110dfdfd74ea48fc42828a2fbf
akun <win11@bzly.home>
2024-01-28 22:26:24 +0800

    fix: tmp

--- group_filter.py
+++ group_filter.py
@@ -10,7 +10,13 @@ from plugins import *
 from channel.chat_channel import check_contain
 from plugins.plugin_chat2db.api_groupx import ApiGroupx
 from plugins.plugin_comm.remark_name_info import RemarkNameInfo
-from plugins.plugin_comm.plugin_comm import EthZero, get_itchat_group, get_itchat_user, make_chat_sign_req
+from plugins.plugin_comm.plugin_comm import (
+    EthZero,
+    get_itchat_group,
+    get_itchat_user,
+    is_valid_string,
+    make_chat_sign_req,
+)
 
 
 # 群白名单二次过滤,确保
@@ -44,12 +50,15 @@ class GroupFilter(Plugin):
 
     def _post_group_msg(self, cmsg):
         wx_user_id = cmsg.actual_user_id
+        wx_user_nickname = cmsg.actual_user_nickname
         wx_group_id = cmsg.other_user_id
         user = get_itchat_user(wx_user_id)
         group = get_itchat_group(wx_group_id)
 
         rm = RemarkNameInfo(user.RemarkName)
         account = rm.get_account()
+        if not is_valid_string(user.NickName):
+            user["NickName"] = wx_user_nickname
         self.groupx.post_chat_record_group_not_at(account, {"agent": self.agent, "user": user, "group": group, "content": cmsg.content})
 
     def on_handle_context(self, e_context: EventContext):
@@ -67,7 +76,7 @@ class GroupFilter(Plugin):
         # 无 @ 也进行处理
         group_name = msg.from_user_nickname
         if group_name not in self.group_white_list:
-            # logger.info(f"--->群名称过滤:群不在白名单中 {group_name}") #频率非常高
+            logger.info(f"--->群名称过滤:群不在白名单中 {group_name}")  # 频率非常高
             e_context.action = EventAction.BREAK_PASS
             return  # 不响应,中止
 
@@ -76,8 +85,12 @@ class GroupFilter(Plugin):
         content = context.content
         match_contain = check_contain(content, self.filter_config.get("group_chat_keyword"))
         if not match_contain:
-            logger.info(f"--->群名称过滤:文本不包含关键词,中止 {content}")
+            logger.info(
+                f"--->grooup filter 无关键字,中止：{content}  {group_name} {msg.actual_user_nickname}"
+            )
             e_context.action = EventAction.BREAK_PASS
             return  # 不响应,中止
 
-        logger.info(f"--->群名称过滤后文本,继续: {content}")  # 转系统及其他插件处理
+        logger.info(
+            f"--->grooup filter 包含关键字,继续:{content} {group_name} {msg.actual_user_nickname}"
+        )  # 转系统及其他插件处理
